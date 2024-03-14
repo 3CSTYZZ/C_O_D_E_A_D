40068b3c71bfde62df701b7df7d878629f15e678
3CSTVZZ <corp.of.alexeev@gmail.com>
2024-03-14 03:03:36 +0300

    dev v1

--- PROJECTS/REACT/Tg bot/webApp/src/App.jsx
+++ PROJECTS/REACT/Tg bot/webApp/src/App.jsx
@@ -1,26 +1,20 @@
 import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
 import "./App.css";
 
-const tg = window.Telegram.WebApp;
 function App() {
-  // forecast for 3 days
-  const apiKey = "bd7c6ee5039741bd911165753242901";
-  const forecast = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=07112&days=3`;
+  const { tg, onToggleButton } = useTelegram();
 
   useEffect(() => {
     tg.ready();
   }, []);
 
-  const onClose = () => {
-    tg.close();
-  };
-
   return (
-    <>
-      <div className="App">
-        <button onClick={onClose}>Close</button>
-      </div>
-    </>
+    <div className="App">
+      <Header />
+      <button onClick={onToggleButton}>Toggle</button>
+    </div>
   );
 }
 
--- PROJECTS/REACT/my-list (to-do)/src/components/UI/input/MyInput.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import classes from "./MyInput.module.css";
-
-export const MyInput = (props) => {
-  return <input {...props} className={classes.myInput}></input>;
-};

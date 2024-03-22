83dd49e1c631354f12b9df9816a3b935de18aec8
3CSTVZZ <corp.of.alexeev@gmail.com>
2024-03-22 14:50:24 +0300

    tg bot dev v1.29

--- PROJECTS/REACT/Tg bot/webApp/src/components/Button/Button.jsx
+++ PROJECTS/REACT/Tg bot/webApp/src/components/Button/Button.jsx
@@ -4,13 +4,5 @@ import "./Button.css";
 export const Button = () => {
   const { onMark } = useTelegram();
 
-  return (
-    <button className={"button"} onClick={onMark}>
-      <img
-        src="../../../src/img/icons8-question-mark-50.png"
-        alt=""
-        className={"qa"}
-      />
-    </button>
-  );
+  return <button className={"button"} onClick={onMark}></button>;
 };

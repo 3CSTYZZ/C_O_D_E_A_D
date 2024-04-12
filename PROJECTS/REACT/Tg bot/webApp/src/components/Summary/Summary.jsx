7e549712efe3915109ce2d4da671508d4b630e05
3CSTVZZ <corp.of.alexeev@gmail.com>
2024-04-13 00:20:19 +0300

    tg bot dev v1.62

--- PROJECTS/REACT/Tg bot/webApp/src/components/Summary/Summary.jsx
+++ PROJECTS/REACT/Tg bot/webApp/src/components/Summary/Summary.jsx
@@ -1,52 +1,125 @@
-import "./Summary.css";
 import { dataset } from "../../data/dataset";
+import { useTelegram } from "../../hooks/useTelegram";
+import "./Summary.css";
+
+const { storage } = dataset();
+const { tg } = useTelegram();
 
 export const Summary = () => {
-  const { storage } = dataset();
-
   return (
     <div className="summary">
       <h3 className="summary__title">Summary</h3>
       <ul className="summary__content">
         <li className="summary__item">
-          <img className="summary__img" src="/img/gym.png" alt="strength" />
-          {storage[0].max[0]}
+          {tg.colorScheme === "light" ? (
+            <img className="summary__img" src="/img/gym.png" alt="strength" />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/gym-dark.png"
+              alt="strength"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[0]}</span>
         </li>
+
         <li className="summary__item">
-          <img className="summary__img" src="/img/running.png" alt="speed" />
-          {storage[0].max[1]}
+          {tg.colorScheme === "light" ? (
+            <img className="summary__img" src="/img/running.png" alt="speed" />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/running-dark.png"
+              alt="speed"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[1]}</span>
         </li>
+
         <li className="summary__item">
-          <img className="summary__img" src="/img/watch.png" alt="reaction" />
-          {storage[0].max[2]}
+          {tg.colorScheme === "light" ? (
+            <img className="summary__img" src="/img/timer.png" alt="reaction" />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/timer-dark.png"
+              alt="reaction"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[2]}</span>
         </li>
+
         <li className="summary__item">
-          <img className="summary__img" src="/img/person.png" alt="jump" />
-          {storage[0].max[3]}
+          {tg.colorScheme === "light" ? (
+            <img className="summary__img" src="/img/person.png" alt="jump" />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/person-dark.png"
+              alt="jump"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[3]}</span>
         </li>
+
         <li className="summary__item">
-          <img
-            className="summary__img"
-            src="/img/volleyball.png"
-            alt="technique"
-          />
-          {storage[0].max[4]}
+          {tg.colorScheme === "light" ? (
+            <img
+              className="summary__img"
+              src="/img/volleyball.png"
+              alt="technique"
+            />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/volleyball-dark.png"
+              alt="technique"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[4]}</span>
         </li>
+
         <li className="summary__item">
-          <img className="summary__img" src="/img/healthy.png" alt="stamina" />
-          {storage[0].max[5]}
+          {tg.colorScheme === "light" ? (
+            <img
+              className="summary__img"
+              src="/img/healthy.png"
+              alt="stamina"
+            />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/healthy-dark.png"
+              alt="stamina"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[5]}</span>
         </li>
+
         <li className="summary__item">
-          <img className="summary__img" src="/img/autism.png" alt="iq" />
-          {storage[0].max[6]}
+          {tg.colorScheme === "light" ? (
+            <img className="summary__img" src="/img/autism.png" alt="iq" />
+          ) : (
+            <img className="summary__img" src="/img/autism-dark.png" alt="iq" />
+          )}
+          <span className="summary__mark">{storage[0].max[6]}</span>
         </li>
+
         <li className="summary__item">
-          <img
-            className="summary__img"
-            src="/img/meditation.png"
-            alt="mental"
-          />
-          {storage[0].max[7]}
+          {tg.colorScheme === "light" ? (
+            <img
+              className="summary__img"
+              src="/img/meditation.png"
+              alt="mental"
+            />
+          ) : (
+            <img
+              className="summary__img"
+              src="/img/meditation-dark.png"
+              alt="mental"
+            />
+          )}
+          <span className="summary__mark">{storage[0].max[7]}</span>
         </li>
       </ul>
     </div>

e29bbb3a15dac9c5485e4846a88b09e80c6808af
3CSTVZZ <corp.of.alexeev@gmail.com>
2024-04-12 20:12:25 +0300

    tg bot dev v1.6

--- PROJECTS/REACT/Tg bot/webApp/src/components/Summary/Summary.jsx
+++ PROJECTS/REACT/Tg bot/webApp/src/components/Summary/Summary.jsx
@@ -9,13 +9,17 @@ export const Summary = () => {
       <h3 className="summary__title">Summary</h3>
       <ul className="summary__content">
         <li className="summary__item">
-          <img className="summary__img" src="/src/img/gym.png" alt="strength" />
+          <img
+            className="summary__img"
+            src="/public/img/gym.png"
+            alt="strength"
+          />
           {storage[0].max[0]}
         </li>
         <li className="summary__item">
           <img
             className="summary__img"
-            src="/src/img/running.png"
+            src="/public/img/running.png"
             alt="speed"
           />
           {storage[0].max[1]}
@@ -23,19 +27,23 @@ export const Summary = () => {
         <li className="summary__item">
           <img
             className="summary__img"
-            src="/src/img/watch.png"
+            src="/public/img/watch.png"
             alt="reaction"
           />
           {storage[0].max[2]}
         </li>
         <li className="summary__item">
-          <img className="summary__img" src="/src/img/person.png" alt="jump" />
+          <img
+            className="summary__img"
+            src="/public/img/person.png"
+            alt="jump"
+          />
           {storage[0].max[3]}
         </li>
         <li className="summary__item">
           <img
             className="summary__img"
-            src="/src/img/volleyball.png"
+            src="/public/img/volleyball.png"
             alt="technique"
           />
           {storage[0].max[4]}
@@ -43,19 +51,19 @@ export const Summary = () => {
         <li className="summary__item">
           <img
             className="summary__img"
-            src="/src/img/healthy.png"
+            src="/public/img/healthy.png"
             alt="stamina"
           />
           {storage[0].max[5]}
         </li>
         <li className="summary__item">
-          <img className="summary__img" src="/src/img/autism.png" alt="iq" />
+          <img className="summary__img" src="/public/img/autism.png" alt="iq" />
           {storage[0].max[6]}
         </li>
         <li className="summary__item">
           <img
             className="summary__img"
-            src="/src/img/meditation.png"
+            src="/public/img/meditation.png"
             alt="mental"
           />
           {storage[0].max[7]}

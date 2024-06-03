e0d04de053d07ba4c39eb0c5a871a1bbbfa16d74
3CSTVZZ <corp.of.alexeev@gmail.com>
2024-06-03 22:20:32 +0300

    tg bot dev v1.71

--- PROJECTS/REACT/Tg bot/webApp/src/data/dataset.js
+++ PROJECTS/REACT/Tg bot/webApp/src/data/dataset.js
@@ -1,58 +1,68 @@
 export function dataset() {
+  function sumArr(arr) {
+    let summary = arr.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      0
+    );
+    return summary;
+  }
+  const max = [10, 8, 10, 9, 9, 9, 9, 7];
+
   // player's pts
   const storage = [
     {
-      max: [10, 8, 10, 9, 8, 9, 9, 9],
+      max: [10, 8, 10, 9, 9, 9, 9, 7],
       tier: "SS",
-      total: 72,
+      // total: 71,
+      total: sumArr(max),
       id: "ecstvzz",
     },
     {
-      tema: [8, 7, 6, 8, 8, 7, 8, 6],
-      tier: "B",
-      total: 58,
+      tema: [7, 7, 7, 7, 8, 7, 8, 8],
+      tier: "A",
+      total: 61,
       id: "temasafonov",
     },
     {
-      fedya: [7, 10, 9, 7, 8, 10, 9, 9],
+      fedya: [7, 10, 9, 7, 7, 10, 8, 6],
       tier: "S",
-      total: 69,
+      total: 66,
       id: "Amblig",
     },
     {
-      barca: [8, 7, 8, 9, 8, 8, 7, 8],
+      barca: [8, 7, 8, 9, 8, 8, 7, 7],
       tier: "A",
-      total: 63,
+      total: 62,
       id: "vladoosssssss",
     },
     {
-      jane: [6, 9, 8, 7, 7, 9, 8, 9],
-      tier: "A",
-      total: 63,
+      jane: [6, 8, 8, 7, 6, 9, 7, 8],
+      tier: "B",
+      total: 59,
       id: "JanineErofeeva",
     },
     {
-      nina: [7, 6, 7, 6, 8, 6, 8, 7],
-      tier: "B",
-      total: 55,
+      nina: [8, 6, 7, 7, 8, 8, 8, 8],
+      tier: "A",
+      total: 60,
       id: "nbulavina",
     },
     {
-      sveta: [7, 7, 8, 6, 8, 8, 8, 7],
-      tier: "B",
-      total: 59,
+      sveta: [8, 7, 8, 7, 7, 8, 8, 7],
+      tier: "A",
+      total: 60,
       id: "antonets_svetlana",
     },
     {
-      paul: [7, 7, 7, 10, 6, 8, 8, 7],
+      paul: [8, 7, 7, 10, 6, 7, 8, 7],
       tier: "A",
-      total: 60,
+      total: 61,
       id: "pavelfedotov99",
     },
     {
-      mario: [7, 8, 8, 8, 9, 7, 9, 7],
+      mario: [7, 8, 9, 8, 9, 19, 8, 9],
       tier: "A",
-      total: 63,
+      total: 70,
       id: "Mincer2412",
     },
     {
@@ -117,6 +127,8 @@ export function dataset() {
     },
     { vlad2: [7, 6, 6, 6, 6, 7, 7, 7], tier: "B", total: 50, id: "SteshVA" },
   ];
+
+  console.log(storage[0].total);
 
   return { storage };
 }
